9d1f50169edb9de36db50cb037e9f71b9d7df7a6
josealcivar <alcivar372@gmail.com>
2022-08-26 18:52:02 -0500

    upload electronapp

---
+++ renderer.js
@@ -0,0 +1,4 @@
+// const information = document.getElementById('info')
+// console.log(informacion)
+// information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+// console.log(informacion)
